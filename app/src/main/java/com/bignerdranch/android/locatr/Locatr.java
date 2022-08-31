c022455c832d9f9fb2f13204589c57956b01be80
Jakub Furtak <kuba.furtak.pl@gmail.com>
2022-08-31 14:31:59 +0200

    repo created

---
+++ app/src/main/java/com/bignerdranch/android/locatr/Locatr.java
@@ -0,0 +1,14 @@
+package com.bignerdranch.android.locatr;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Locatr extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
